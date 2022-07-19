1e221fc37e92eeba97781984315507c2fe639166
sarthakaushal <sarthakkaushal@outlook.com>
2022-07-19 18:07:35 +0530

    Editted DS path

--- ml_service/pipelines/customer_churn_build_train_pipeline.py
+++ ml_service/pipelines/customer_churn_build_train_pipeline.py
@@ -64,7 +64,7 @@ def main():
         # create_sample_data_csv()
 
         # Use a CSV to read in the data set.
-        file_name = "customer_churn_proc.csv"
+        file_name = "data/customer_churn_proc.csv"
 
         if not os.path.exists(file_name):
             raise Exception(
